ae1ac491874f132b13aab78478d422574a9ea35e
RawzaDigital <rawzadigital@gmail.com>
2019-08-15 09:44:25 +0700

    add onClickListener to button set and cancel sheduler

--- app/src/main/java/net/kampungweb/mygcmnetworkmanager/MainActivity.java
+++ app/src/main/java/net/kampungweb/mygcmnetworkmanager/MainActivity.java
@@ -3,12 +3,26 @@ package net.kampungweb.mygcmnetworkmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnSetScheduler, btnCancelScheduler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCancelScheduler = findViewById(R.id.btn_cancel_scheduler);
+        btnSetScheduler = findViewById(R.id.btn_set_scheduler);
+        btnSetScheduler.setOnClickListener(this);
+        btnCancelScheduler.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
     }
 }
