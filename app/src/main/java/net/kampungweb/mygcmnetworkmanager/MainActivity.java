3bbd11e17a5205cbc70eece60baa4c75ab89ad43
RawzaDigital <rawzadigital@gmail.com>
2019-08-15 09:28:30 +0700

    Initial commit

---
+++ app/src/main/java/net/kampungweb/mygcmnetworkmanager/MainActivity.java
@@ -0,0 +1,14 @@
+package net.kampungweb.mygcmnetworkmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
